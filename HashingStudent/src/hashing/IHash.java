697e26caa6086de6a62ccb20e34b9453fa0b52d7
RandomDigraph <77731335+RandomDigraph@users.noreply.github.com>
2021-02-05 11:12:22 +0000

    Add files via upload

---
+++ HashingStudent/src/hashing/IHash.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hashing;
+
+import java.lang.String;
+/**
+ *
+ * @author dw
+ */
+public interface IHash {
+    void add(int Key, String Value);
+    void delete(int Key);
+    String item(int Key);
+    boolean contains(int Key);
+    int length();
+    boolean isEmpty();    
+}
